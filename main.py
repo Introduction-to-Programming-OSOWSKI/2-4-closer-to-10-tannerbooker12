c8fd0eb617871fd3e81e7754701ca25c834806a1
tannerbooker12 <829075@mystma.org>
2022-09-21 08:54:25 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x,y):
+    if 10-x>10-y: 
+        return(x)
+    elif 10-y>x-10:
+            return(y)
+    else:
+        x=y 
+        return("0")
+
+print(close10(9,12))
